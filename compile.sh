560e6010cbf582c8710679d41fb7ff2bc5f3a16b
Christian Bahls <asm2amqp@qb352.de>
2025-07-05 07:06:32 +0200

    Update compile.sh
    Fix the escaping of shell variables

--- compile.sh
+++ compile.sh
@@ -10,13 +10,13 @@ HOST=localhost
 PORT=5672
 
 nasm \
-   -DUSERNAME='"${USERNAME}"' \
-   -DPASSWORD='"${PASSWORD}"' \
-   -DEXCHANGE='"${EXCHANGE}"' \
-   -DROUTINGKEY='"${ROUTINGKEY}"' \
-   -DQUEUENAME='"${QUEUENAME}"' \
-   -DVHOST='"${VHOST}"' \
-   -DHOST='"${HOST}"' \
+   -DUSERNAME='"'"${USERNAME}"'"' \
+   -DPASSWORD='"'"${PASSWORD}"'"' \
+   -DEXCHANGE='"'"${EXCHANGE}"'"' \
+   -DROUTINGKEY='"'"${ROUTINGKEY}"'"' \
+   -DQUEUENAME='"'"${QUEUENAME}"'"' \
+   -DVHOST='"'"${VHOST}"'"' \
+   -DHOST='"'"${HOST}"'"' \
    -DPORT='${PORT}' \
    -f elf64 -o amqp.o amqp.asm && \
 ld -dynamic-linker /lib/ld-musl-x86_64.so.1 -lc amqp.o -o amqp
